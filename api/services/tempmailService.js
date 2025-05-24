34d4eb524c7b539c90a455eb02a9fd7b6fd55ca2
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-05-24 12:34:21 +0100

    Update tempmailService.js

--- api/services/tempmailService.js
+++ api/services/tempmailService.js
@@ -1,1 +1,20 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+import tough from 'tough-cookie'
+import { wrapper } from 'axios-cookiejar-support'
 
+const cookieJar = new tough.CookieJar()
+const client = wrapper(axios.create({ jar: cookieJar, withCredentials: true }))
+
+export async function getTempMail() {
+  const res = await client.get('https://temp-mail.org/en/')
+  const $ = cheerio.load(res.data)
+  const email = $('#mail').val() || $('input#mail').attr('value')
+  if (!email) throw new Error('Failed to get temp mail')
+  return email
+}
+
+export async function getInboxMessages(email) {
+  // Requires real API, so placeholder
+  return [`Inbox fetch for ${email} requires API access`]
+}
